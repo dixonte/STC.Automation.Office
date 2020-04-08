4b52c079205c1f4ef82afd0931db257790a36414
Trent Dixon <tdixon@pittsh.com.au>
2020-04-08 13:53:43 +1000

    Forgot to rename the enum members

--- STC.Automation.Office/Core/Enums/ThemeColorIndex.cs
+++ STC.Automation.Office/Core/Enums/ThemeColorIndex.cs
@@ -12,91 +12,91 @@ namespace STC.Automation.Office.Core.Enums
         /// <summary>
         /// Specifies no theme color.
         /// </summary>
-        msoNotThemeColor = 0,
+        NotThemeColor = 0,
 
         /// <summary>
         /// Specifies the Accent 1 theme color.
         /// </summary>
-        msoThemeColorAccent1 = 5,
+        Accent1 = 5,
 
         /// <summary>
         /// Specifies the Accent 2 theme color.
         /// </summary>
-        msoThemeColorAccent2 = 6,
+        Accent2 = 6,
 
         /// <summary>
         /// Specifies the Accent 3 theme color.
         /// </summary>
-        msoThemeColorAccent3 = 7,
+        Accent3 = 7,
 
         /// <summary>
         /// Specifies the Accent 4 theme color.
         /// </summary>
-        msoThemeColorAccent4 = 8,
+        Accent4 = 8,
 
         /// <summary>
         /// Specifies the Accent 5 theme color.
         /// </summary>
-        msoThemeColorAccent5 = 9,
+        Accent5 = 9,
 
         /// <summary>
         /// Specifies the Accent 6 theme color.
         /// </summary>
-        msoThemeColorAccent6 = 10,
+        Accent6 = 10,
 
         /// <summary>
         /// Specifies the Background 1 theme color.
         /// </summary>
-        msoThemeColorBackground1 = 14,
+        Background1 = 14,
 
         /// <summary>
         /// Specifies the Background 2 theme color.
         /// </summary>
-        msoThemeColorBackground2 = 16,
+        Background2 = 16,
 
         /// <summary>
         /// Specifies the Dark 1 theme color.
         /// </summary>
-        msoThemeColorDark1 = 1,
+        Dark1 = 1,
 
         /// <summary>
         /// Specifies the Dark 2 theme color.
         /// </summary>
-        msoThemeColorDark2 = 3,
+        Dark2 = 3,
 
         /// <summary>
         /// Specifies the theme color for a clicked hyperlink.
         /// </summary>
-        msoThemeColorFollowedHyperlink = 12,
+        FollowedHyperlink = 12,
 
         /// <summary>
         /// Specifies the theme color for a hyperlink.
         /// </summary>
-        msoThemeColorHyperlink = 11,
+        Hyperlink = 11,
 
         /// <summary>
         /// Specifies the Light 1 theme color.
         /// </summary>
-        msoThemeColorLight1 = 2,
+        Light1 = 2,
 
         /// <summary>
         /// Specifies the Light 2 theme color.
         /// </summary>
-        msoThemeColorLight2 = 4,
+        Light2 = 4,
 
         /// <summary>
         /// Specifies a mixed color theme.
         /// </summary>
-        msoThemeColorMixed = -2,
+        Mixed = -2,
 
         /// <summary>
         /// Specifies the Text 1 theme color.
         /// </summary>
-        msoThemeColorText1 = 13,
+        Text1 = 13,
 
         /// <summary>
         /// Specifies the Text 2 theme color.
         /// </summary>
-        msoThemeColorText2 = 15
+        Text2 = 15
     }
 }
